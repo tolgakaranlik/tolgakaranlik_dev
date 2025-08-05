5411a70e7ac472162cb99c3290205fec2cc818e5
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 13:35:41 +0300

    Update addComment.ts

--- src/app/blog/actions/addComment.ts
+++ src/app/blog/actions/addComment.ts
@@ -9,7 +9,7 @@ export type State = {
   error: string[] | null;
 };
 
-export async function addComment(prevState: State, formData: FormData) {
+export async function addComment(prevState: any, formData: FormData) {
 
   const commentSchema = z.object({
     postId: z.string(),
