ee25e674144cf3c2ad2fcd8893cc0ec0d7f91971
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 15:27:27 +0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+import { User } from '@/lib/data';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
