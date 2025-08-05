a56a96a427f24498d6f20c885ea7d52c206cb166
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 16:05:12 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
 export default config;
