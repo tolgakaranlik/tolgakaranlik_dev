1af448c14831a60e7ee48beccf2e3e6be7368736
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 15:45:07 +0300

    Update page.tsx

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -5,19 +5,20 @@ import Link from 'next/link'
 export default async function AdminUsers() {
   const user = await adminTest();
   const users = await getUsers();
+  const userRole = user == null ? "0" : user?.role;
 
   return (
     <>
 	  <div>Users</div>
 	  <br />
 	  <Users>
-	  {users.map((u) => (
-		<User key={u.id} id={u.id} name={u.name} email={u.email} role={u.role == '1' ? 'Admin' : 'Guest'} canEdit={user.role} />
+	  {users?.map((u) => (
+		<User key={u.id} id={u.id} name={u.name} email={u.email} role={u.role == '1' ? 'Admin' : 'Guest'} canEdit={userRole} />
 	  ))}
 	  </Users>
 
 	  <br />
-	  {user.role == "0" && (
+	  {userRole == "0" && (
 	  <>
 	  <p className="text-red-500">Guest accounts cannot add new entries</p>
 	  <br />
@@ -25,7 +26,7 @@ export default async function AdminUsers() {
 	  </>
 	  )}
 
-	  {user.role == "1" && (
+	  {userRole == "1" && (
 	  <Link href="./users/add" className="rounded-md h-[50px] w-58 p-4 bg-gray-600 hover:bg-gray-500 text-center text-white cursor-pointer">Add New User</Link>
 	  )}
     </>
