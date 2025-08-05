f56ca4aeb91a07c362108a1820159d9b21b88279
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 17:18:07 +0300

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
-  const session = await auth();
+  //const session = await auth();
   
   return (
     <div className="min-h-screen flex">
