29438dc98f5761515b34e10d09402caa61416c45
Tolga Karanlikoglu <tolgakaranlik@gmail.com>
2025-08-05 15:47:39 +0300

    Update BlogPostFullPage.tsx

--- src/components/BlogPostFullPage.tsx
+++ src/components/BlogPostFullPage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { lusitana, robotoMono } from '@/components/ui/fonts';
-import { CommentForm } from '@/app/blog/CommentForm.tsx';
+import { CommentForm } from '@/app/blog/CommentForm';
 
 export function BlogPostFullPage({ id, title, date, tags, authorName, authorIcon, cover, children }: { id, title, date, tags, authorName, authorIcon, cover: string; children: React.ReactNode })
 {
